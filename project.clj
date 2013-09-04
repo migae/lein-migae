1718ff26f18edc448f888f07595c1182964f6178
Gregg Reynolds <dev@mobileink.com>
2013-09-04 04:59:39 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject lein-migae "0.1.4"
+(defproject lein-migae "0.1.6-SNAPSHOT"
   :description "Leiningen plugin for migae sdk"
   :url "http://github.com/greynolds/lein-migae"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :checksum :ignore
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [leiningen "2.3.1"]
                  [org.clojure/tools.logging "0.2.3"]
